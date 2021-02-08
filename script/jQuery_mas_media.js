13a0142e70647f61ff45edd63254944e420d0591
AnaMariaHuguet <65618218+AnaMariaHuguet@users.noreply.github.com>
2021-02-08 23:11:43 +0100

    Add files via upload

---
+++ script/jQuery_mas_media.js
@@ -0,0 +1,28 @@
+﻿$(function () {// abreviatura de $(document).ready(function () 
+
+    function update() {   
+        //var mql = window.matchMedia("(orientation: portrait)");
+        var mql1 = window.matchMedia("(min-width:300px)");
+        var mql2 = window.matchMedia("(min-width:800px)");
+        var mql3 = window.matchMedia("(min-width:1200px)");
+                  
+        if (mql3.matches){
+            $("main h2:first-child").text("PC Screen");
+                }  
+            else if (mql2.matches) {
+                $("main h2:first-child").text("Tablet");
+                }else if (mql1.matches) {
+                    $("main h2:first-child").text("Smartphone");
+            
+            }
+   
+    $("h3").text("Viewport Size....." + $(window).width() + " x " + $(window).height() );    
+}
+    update();
+
+    /* Window event. Resize browser window */
+    /* resize= cuando cambia tamaño de la ventana*/
+    $(window).resize(function () { update(); });
+});
+
+
